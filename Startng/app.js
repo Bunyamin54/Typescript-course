c6da1eb187f6a8ffd6e10ca170a3b6c5ff309748
Bunyamin Genc <bunyaming@uia.no>
2024-08-24 10:40:00 +0200

    variabler

--- Startng/app.js
+++ Startng/app.js
@@ -1,3 +1,11 @@
+var firstName = " Can";
+var lastName = " Boz";
+var age = 29;
+var isUpdated = true;
 console.log("Typescripte girdik");
 console.log("Can Boz");
 console.log("Egitimimize hosgeldiniz");
+//* type language typscript - age degerine string atayamayiz
+function display(id, name) {
+    console.log("Id = " + id + ", Name =" + name);
+}
--- Startng/app.ts
+++ Startng/app.ts
@@ -1,3 +1,21 @@
+
+let firstName: string = " Can";
+let lastName: string = " Boz"
+
+let age: number = 29;
+let isUpdated : boolean = true;
+
+
 console.log("Typescripte girdik");
 console.log("Can Boz");
 console.log("Egitimimize hosgeldiniz");
+
+
+//* type language typscript - age degerine string atayamayiz
+
+
+
+function display (id: any, name:string) {
+
+    console.log("Id = " + id + ", Name =" + name)
+}
