a7c1b33465cff10d4a0090dab4dd4dc79170100f
Bunyamin Genc <bunyaming@uia.no>
2024-08-24 20:20:39 +0200

    inference

--- Startng/app.ts
+++ Startng/app.ts
@@ -246,7 +246,7 @@ do {
 
     //* function
 
-     function add (a: number , b: number): number{
+     function add (a: number , b: number): number{  //* inference  burdaki number
         return a+b
      }
 
